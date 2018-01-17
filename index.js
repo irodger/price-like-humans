a4abb8518b8c9d5bbda8ecb7688e9f0b6acd638d
irodger <raxennt@gmail.com>
2018-01-17 23:15:42 +0300

    Add missed util function & rename general function

--- index.js
+++ index.js
@@ -1,9 +1,21 @@
-function setNumberFormat(value, separator) {
+function getHumansPrice(value, separator) {
   const stringSeparator = separator || '.';
   const numberArray = value.toString().split(stringSeparator);
   const regexpWithSpace = /\B(?=(\d{3})+(?!\d))/g;
   const numberBeforeDot = numberArray[0].replace(regexpWithSpace, ' ');
 
+  function reverseThis(item) {
+    if (typeof item === 'string') {
+      return item.split('').reverse().join('');
+    }
+
+    if (typeof item === 'number') {
+      return Number(item.toString().split('').reverse().join(''));
+    }
+
+    return null;
+  }
+  
   if (numberArray.length > 1) {
       const reversedNumberAfterDot = reverseThis(numberArray[1]).replace(regexpWithSpace, ' ');
       const numberAfterDot = reverseThis(reversedNumberAfterDot);
